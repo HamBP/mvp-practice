3b52b828ba97d6c6ad3b7531bd82e882219f8401
algosketch <algosketch@gmail.com>
2024-07-16 22:16:52 +0900

    refactor: main activity 패키지 변경

--- app/src/main/java/me/algosketch/mvpground/controller/MainActivity.kt
+++ app/src/main/java/me/algosketch/mvpground/controller/MainActivity.kt
@@ -1,10 +1,11 @@
-package me.algosketch.mvpground
+package me.algosketch.mvpground.controller
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import me.algosketch.mvpground.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
