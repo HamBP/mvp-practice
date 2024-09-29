e5b87ac216b2f15cc2575eb9e03b7b14f3379a9e
algosketch <algosketch@gmail.com>
2024-09-30 02:47:29 +0900

    docs : 주석 추가

--- app/src/main/java/me/algosketch/mvpground/controller/MainActivity.kt
+++ app/src/main/java/me/algosketch/mvpground/controller/MainActivity.kt
@@ -14,6 +14,9 @@ import kotlinx.coroutines.flow.onEach
 import me.algosketch.mvpground.R
 import me.algosketch.mvpground.model.TodoRepository
 
+/**
+ * MVC의 Controller에서 사용자 입력을 받아 model과 상호작용하고 결과를 View에 전달한다.
+ */
 class MainActivity : AppCompatActivity() {
     private lateinit var adapter: TodoAdapter
     private val repository by lazy { TodoRepository() }
@@ -37,19 +40,22 @@ class MainActivity : AppCompatActivity() {
         val recyclerView = findViewById<RecyclerView>(R.id.rv_todos)
         recyclerView.adapter = adapter
         val submitButton = findViewById<Button>(R.id.btn_submit)
-        submitButton.setOnClickListener {
+        submitButton.setOnClickListener { // 사용자 입력을 처리하므로 Controller 로직
             addTask()
         }
     }
 
     private fun initData() {
-        repository.getTodos()
+        repository.getTodos() // 모델에서 데이터 호출
             .onEach { newTodos ->
                 adapter.submitList(newTodos)
             }
             .launchIn(lifecycleScope)
     }
 
+    /**
+     * 사용자 입력을 받고(Controller 역할) Model에 전달
+     */
     private fun addTask() {
         val newTaskEditText = findViewById<EditText>(R.id.et_new_task)
         val newTask = newTaskEditText.text.toString()
